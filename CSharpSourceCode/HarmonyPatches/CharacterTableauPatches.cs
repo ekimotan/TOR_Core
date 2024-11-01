c2d5c818c58809c43074c1f4295b2929d7fa592d
Linus Tiemann <linus-tiemann@live.de>
2024-11-01 20:54:24 +0100

    fix patch

--- CSharpSourceCode/HarmonyPatches/CharacterTableauPatches.cs
+++ CSharpSourceCode/HarmonyPatches/CharacterTableauPatches.cs
@@ -4,18 +4,18 @@ using TaleWorlds.MountAndBlade.View.Tableaus;
 
 namespace TOR_Core.HarmonyPatches;
 
-public class CharacterTableauPatches
+[HarmonyPatch]
+public static class CharacterTableauPatches
 {
-    [HarmonyPatch(typeof(CharacterTableau), nameof(CharacterTableau.SetRace))]
-    public class SetRacePostFix
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CharacterTableau), "SetRace")]
+    public static void SetRacePostfix(CharacterTableau __instance)
     {
-        static void Postfix(CharacterTableau __instance)
-        {
-            var _agentVisuals = AccessTools.Field(typeof(CharacterTableau), "_agentVisuals")?.GetValue(__instance) as AgentVisuals;
-            _agentVisuals?.Reset();
-            var _oldAgentVisuals = AccessTools.Field(typeof(CharacterTableau), "_oldAgentVisuals")?.GetValue(__instance) as AgentVisuals;
-            _oldAgentVisuals?.Reset();
-            AccessTools.Method(typeof(CharacterTableau), "InitializeAgentVisuals").Invoke(__instance, new object[] { });
-        }
+        var _agentVisuals = AccessTools.Field(typeof(CharacterTableau), "_agentVisuals")?.GetValue(__instance) as AgentVisuals;
+        _agentVisuals?.Reset();
+        var _oldAgentVisuals = AccessTools.Field(typeof(CharacterTableau), "_oldAgentVisuals")?.GetValue(__instance) as AgentVisuals;
+        _oldAgentVisuals?.Reset();
+        AccessTools.Method(typeof(CharacterTableau), "InitializeAgentVisuals").Invoke(__instance, new object[] { });
     }
+
 }
